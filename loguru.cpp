bd9dbb04ec4801cb954e16072f7fd15835030e36
sailfish009 <sailfish009@gmail.com>
2016-09-09 20:13:23 +0900

    fix compile error in vs2013

--- loguru.cpp
+++ loguru.cpp
@@ -469,6 +469,36 @@ namespace loguru
 		set_fatal_handler(nullptr);
 	}
 
+#if _MSC_VER < 1900
+
+#define snprintf c99_snprintf
+#define vsnprintf c99_vsnprintf
+
+	__inline int c99_vsnprintf(char *outBuf, size_t size, const char *format, va_list ap)
+	{
+		int count = -1;
+
+		if (size != 0)
+			count = _vsnprintf_s(outBuf, size, _TRUNCATE, format, ap);
+		if (count == -1)
+			count = _vscprintf(format, ap);
+
+		return count;
+	}
+
+	__inline int c99_snprintf(char *outBuf, size_t size, const char *format, ...)
+	{
+		int count;
+		va_list ap;
+
+		va_start(ap, format);
+		count = c99_vsnprintf(outBuf, size, format, ap);
+		va_end(ap);
+
+		return count;
+	}
+#endif
+
 	void write_date_time(char* buff, size_t buff_size)
 	{
 		auto now = system_clock::now();
